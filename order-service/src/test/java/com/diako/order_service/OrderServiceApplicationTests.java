57c76cfb063cbb28b8d97f18fee4a2b4d3115760
DiakoIsmail <ismaildiako@gmail.com>
2026-07-05 17:40:41 +0200

    Add GitHub Actions CI workflow

--- order-service/src/test/java/com/diako/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/diako/order_service/OrderServiceApplicationTests.java
@@ -1,23 +1,15 @@
-package com.diako.order_service.config;
+package com.diako.order_service;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.kafka.KafkaContainer;
+import com.diako.order_service.config.TestcontainersConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-@TestConfiguration(proxyBeanMethods = false)
-public class TestcontainersConfiguration {
+@SpringBootTest
+@Import(TestcontainersConfiguration.class)
+class OrderServiceApplicationTests {
 
-	@Bean
-	@ServiceConnection(name = "postgresql")
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>("postgres:18");
-	}
-
-	@Bean
-	@ServiceConnection
-	KafkaContainer kafkaContainer() {
-		return new KafkaContainer("apache/kafka:latest");
+	@Test
+	void contextLoads() {
 	}
 }
--- order-service/src/test/java/com/diako/order_service/config/TestcontainersConfiguration.java
+++ order-service/src/test/java/com/diako/order_service/config/TestcontainersConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.kafka.KafkaContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
@@ -12,5 +13,11 @@ public class TestcontainersConfiguration {
     @ServiceConnection(name = "postgresql")
     PostgreSQLContainer<?> postgresContainer() {
         return new PostgreSQLContainer<>("postgres:18");
+    }
+
+    @Bean
+    @ServiceConnection
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer("apache/kafka:latest");
     }
 }
